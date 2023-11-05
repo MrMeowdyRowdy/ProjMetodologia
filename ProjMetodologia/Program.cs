e4238fb8aeac7fc030fc6b80b3548ccfa98709c1
Xavi122323 <xavier.ramos@udla.edu.ec>
2023-11-05 01:20:23 -0500

    Index y create de centro de costos

--- ProjMetodologia/Program.cs
+++ ProjMetodologia/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<SharedStateService>();
+builder.Services.AddSingleton<CentroCostos>();
 
 builder.Services.AddScoped(sp =>
     new HttpClient
