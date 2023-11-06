4633a2c68619f838d7c651df603d96af5762f24a
Adrián Bedón <arbedon2002@gmail.com>
2023-11-06 18:58:20 -0500

    Movimiento Planilla Create & Update

---
+++ ProjMetodologia/Data/MovimientoPlanilla/MovimientoE3.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjMetodologia.Data.MovimientoPlanilla
+{
+    public class MovimientoE3
+    {
+        public string CodigoMovimientoExce {  get; set; }
+        public string DesripMovimientoExce { get; set; }
+    }
+}
---
+++ ProjMetodologia/Data/MovimientoPlanilla/TipoOperacion.cs
@@ -0,0 +1,8 @@
+﻿namespace ProjMetodologia.Data.MovimientoPlanilla
+{
+    public class TipoOperacion
+    {
+        public int CodigoTipooperacion { get; set; }
+        public string NombreOperacion { get; set; }
+    }
+}
