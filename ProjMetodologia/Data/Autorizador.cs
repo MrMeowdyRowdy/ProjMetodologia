cff4a654e9e969d81dd8d7efb65c3ca628beecd3
Adrián Bedón <arbedon2002@gmail.com>
2023-11-17 23:16:53 -0500

    Cambios Implementados

---
+++ ProjMetodologia/Data/Autorizador.cs
@@ -0,0 +1,18 @@
+﻿namespace ProjMetodologia.Data
+{
+    public class Autorizador
+    {
+        public string NOMBREUSUARIO { get; set; }
+        public object PERFIL { get; set; }
+        public string OBSERVACION { get; set; }
+        public object CODIGOPERFIL { get; set; }
+        public object ESTADO { get; set; }
+        public object COMPANIA { get; set; }
+        public object Emisor { get; set; }
+        public object Cargo { get; set; }
+        public object NOMBREEMISOR { get; set; }
+        public object NOMBRECOMPANIA { get; set; }
+        public object USUARIOCLIENTE { get; set; }
+        public object RucUsuario { get; set; }
+    }
+}
---
+++ ProjMetodologia/Data/GestionNomina/GestionNomina.cs
@@ -0,0 +1,16 @@
+﻿namespace ProjMetodologia.Data.GestionNomina
+{
+    public class GestionNomina
+    {
+        public int Sucursal { get; set; }
+        public int CodigoConceptoNomina { get; set; }
+        public string DescripcionConcepto { get; set; }
+        public int CodigoCategoriaocupacional { get; set; }
+        public string DescripcionCategoria { get; set; }
+        public string CodigoOperacion { get; set; }
+        public double CodigoCuentaContable { get; set; }
+        public string CodigoTipoCuenta { get; set; }
+        public string DescripcionCuenta { get; set; }
+        public object Mensaje { get; set; }
+    }
+}
